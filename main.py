3ade1ad5c5c27ec5d2c5139801256559ba96f938
Prashin Jignesh Parikh <prashinjigneshparikh@Prashins-MacBook-Pro.local>
2024-11-16 20:28:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# We can run the streamlit application by typing the following command in terminal: `streamlit run ./main.py`
+# It will then run a webserver for us on the port no 8501
+# Anytime something must be updated on the screen, Streamlit reruns the entire Python script from top to bottom
+import streamlit as st
+
+# `write()` function is used to write/add anything to the web app
+st.write("Hello Prashin!")
+st.write({"key": "Modi"})
+st.write(True)
+st.write([1, 2, "narendra", "park"])
+3 + 4 - 11
+
+"hEELLO World"
+
+
