09be320285a8fe386eecc5a8614458ecb3a438c9
Prashin Jignesh Parikh <prashinparikh99@gmail.com>
2024-11-17 13:21:13 +0530

    using buttons in streamlit

--- main.py
+++ main.py
@@ -3,13 +3,8 @@
 # Anytime something must be updated on the screen, Streamlit reruns the entire Python script from top to bottom
 import streamlit as st
 
-# `write()` function is used to write/add anything to the web app
-st.write("Hello Prashin!")
-st.write({"key": "Modi"})
-st.write(True)
-st.write([1, 2, "narendra", "park"])
-3 + 4 - 11
+pressed = st.button("Press me")
+print("First:", pressed)
 
-"hEELLO World"
-
-
+pressed = st.button("Second button")
+print("Second:", pressed)
