6aebfb90377b414d716ae28a433c61b51d7d6534
Prashin Jignesh Parikh <prashinparikh99@gmail.com>
2024-11-18 11:45:04 +0530

    add advanced form example

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import streamlit as st
 from datetime import datetime
 
-st.title("User Information Form")
+min_date = datetime(1990, 1, 1)
+max_date = datetime.now()
 
-form_values = {"name": None, "height": None, "gender": None, "dob": None}
+st.title("user information form".capitalize())
 
+# key is used to uniquely identify the form
+# Only after the submit button is pressed, the code reruns and updates the state
 with st.form(key="user_info_form", clear_on_submit=True):
-    form_values["name"] = st.text_input("Enter your name")
-    form_values["height"] = st.number_input(
-        "Enter your height (cms)", step=1, min_value=0, max_value=250
-    )
-    form_values["gender"] = st.selectbox(
-        "Select your gender", ["Male", "Female", "Other"]
-    )
-    form_values["dob"] = st.date_input(
-        "Enter your date of birth",
-        min_value=datetime(1940, 1, 1),
-        max_value=datetime.now(),
+    name1 = st.text_input("Enter your first name")
+    birth_date = st.date_input(
+        "Enter your birth date", min_value=min_date, max_value=max_date
     )
 
-    submit_button = st.form_submit_button(label="Submit")
-    print("after submit")
-    if submit_button:
-        print("in if")
-        if not all(form_values.values()):
-            st.warning("Please fill in all of the fields")
+    if birth_date:
+        age = max_date.year - birth_date.year
+        if (birth_date.month > max_date.month) or (
+            birth_date.month == max_date.month and birth_date.day > max_date.day
+        ):
+            age -= 1
+        st.write(f"Your calculated age is {age} years!")
+
+    submit_button1 = st.form_submit_button(label="Submit Form")
+
+    if submit_button1:
+        if not name1 or not birth_date:
+            st.warning("Please fill out all of the form fields!")
         else:
+            st.success(f"Thank you, {name1}. You age is {age}.")
             st.balloons()
-            st.write("### Information")
-            for key, val in form_values.items():
-                st.write(f"{key.capitalize()}: {val}")
